d120f6605aee4889ac3126bf0fb010cf239c06d2
niki-sal <ne.salehi@gmail.com>
2021-02-05 22:38:24 -0800

    adding image post route

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
 const multer = require('multer')
 const cloudinary = require('cloudinary')
+//upload for images, make a uploads folder, pass through the route as middleware
+const uploads = multer({ dest: './uploads'})
 
 const app = express()
-app.set ('view engine', 'ejs')
+app.set('view engine', 'ejs')
 
 app.use(ejsLayouts)
 app.use(express.urlencoded({extended: false})) // able to access req.body
@@ -14,6 +16,26 @@ app.use(express.urlencoded({extended: false})) // able to access req.body
 app.get('/', function (req, res){
     res.send('welcome to my express Cloudinary App')
 })
+
+// app.get ('/images', (req, res)=>{
+//     res.render('index')
+// })
+
+app.get('/images/new',(req, res)=>{
+    res.render('new')
+})
+
+app.post('/images', uploads.single('inputFile'),(req,res)=>{
+    //grab the uploaded file
+    const image = req.file.path
+    console.log(image)
+    cloudinary.uploader.upload(image, (result) =>{
+        //results that comes back from cloudinary
+        console.log (result)
+        res.render('index',{ image : result.url })
+    })
+})
+
 
 //listen on PORT
 const PORT = process.env.PORT || 8000
